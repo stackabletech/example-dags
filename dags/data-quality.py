452578b00c6d09446ba4314ddd88c6bed455be43
Sönke Liebau <soenke.liebau@stackable.tech>
2024-09-06 10:21:52 +0200

    fixes

--- dags/data-quality.py
+++ dags/data-quality.py
@@ -1,6 +1,8 @@
 from airflow import DAG
 from great_expectations_provider.operators.great_expectations import GreatExpectationsOperator
 from datetime import datetime
+from pathlib import Path
+from airflow.models.baseoperator import chain
 
 # Define default arguments for the DAG
 default_args = {
@@ -10,29 +12,23 @@ default_args = {
 }
 
 base_path = Path(__file__).parents[2]
-data_dir = base_path / "include" / "data"
-data_file = data_dir / "yellow_tripdata_sample_2019-01.csv"
+data_dir = base_path / "data" / "data-quality"
 
-ge_root_dir = str(base_path / "include" / "great_expectations")
+ge_root_dir = str(base_path / "checkpoints")
 
-
-# Instantiate the DAG
 with DAG(
-        dag_id='great_expectations_example',  # Name of the DAG
-        default_args=default_args,
-        schedule_interval='@daily',  # Schedule to run daily
-        catchup=False,  # Prevent running for past dates if start_date is in the past
+        dag_id="example_great_expectations_dag",
+        start_date=datetime(2021, 12, 15),
+        catchup=False,
+        schedule_interval=None,
 ) as dag:
-
-    # Define a task using GreatExpectationsOperator to run a validation checkpoint
-    ge_checkpoint_task = GreatExpectationsOperator(
-        task_id='ge_checkpoint_validation',  # Unique ID for the task
-        checkpoint_name='my_checkpoint',  # Name of your Great Expectations checkpoint
-        data_context_root_dir='/path/to/great_expectations',  # Path to your Great Expectations project
-        fail_task_on_validation_failure=True,  # Fail the task if validation fails
-        validation_operator_name='action_list_operator'  # Name of the validation operator in your GE config
+    ge_data_context_root_dir_with_checkpoint_name_pass = GreatExpectationsOperator(
+        task_id="ge_data_context_root_dir_with_checkpoint_name_pass",
+        data_context_root_dir=ge_root_dir,
+        checkpoint_name="trino_gdifr_checkpoint.yml",
     )
 
-    # Add additional tasks or dependencies here if needed
 
-# The DAG is automatically picked up by Airflow as it is defined within the "with" block.
+chain(
+    ge_data_context_root_dir_with_checkpoint_name_pass,
+)
